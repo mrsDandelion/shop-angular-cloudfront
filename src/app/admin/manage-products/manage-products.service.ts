eb05543a6d9c026f74c98982fdd6e20157e9d644
Darya Drachuk <darya_drachuk@epam.com>
2023-11-06 23:49:38 +0300

    feat: task-7

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -3,6 +3,7 @@ import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
 import { UrlResponse } from './manage-products.interface';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -35,10 +36,18 @@ export class ManageProductsService extends ApiService {
   private getPreSignedUrl(fileName: string): Observable<UrlResponse> {
     const url = this.getUrl('import', 'import');
 
+    const authorizationToken = localStorage.getItem('authorization_token');
+
+    const headers = new HttpHeaders({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      Authorization: `Basic ${authorizationToken}`,
+    });
+
     return this.http.get<UrlResponse>(url, {
       params: {
         name: fileName,
       },
+      headers,
     });
   }
 }
